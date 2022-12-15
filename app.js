22522d9631d453caf1df6cebaa0974260ee07362
GatharikiH <sgatharikih@gmail.com>
2022-12-15 21:35:51 +0300

    added cors middleware

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 let express = require('express');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let cors = require('cors');
 
 let indexRouter = require('./routes/indexRouter');
 
 let app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
